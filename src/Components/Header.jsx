743f62c1490e1ae32326590886a1888ff8141768
yashdhameliya88 <yashdhameliya98@gmail.com>
2024-05-26 16:07:13 +0530

    server-win add

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import img1 from './download.png'
+import myFile from './../Files/server-win.exe'
 const Header = () => {
+
+  const handleDownload = () => {
+    // Create a link element
+    const link = document.createElement('a');
+    // Set the href attribute to the image file path
+    link.href = myFile;
+    // Set the download attribute to force download
+    link.download = 'server-win.exe';
+    // Simulate a click on the link
+    document.body.appendChild(link);
+    link.click();
+    // Cleanup: remove the link element
+    document.body.removeChild(link);
+  };
   return (
     <div>
       <header className='flex border-b py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50'>
@@ -34,7 +49,7 @@ const Header = () => {
         </ul>
       </div>
   
-      {/* <button id="toggleOpen" className='lg:hidden ml-auto'>
+      <button id="toggleOpen" className='lg:hidden ml-auto'>
         <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd"
             d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
@@ -44,17 +59,10 @@ const Header = () => {
   
       <div className='flex lg:ml-auto max-lg:w-full'>
         <div
-          className='flex xl:w-80 max-xl:w-full bg-gray-100 px-6 py-3 rounded outline outline-transparent focus-within:outline-[#007bff] focus-within:bg-transparent'>
-          <input type='text' placeholder='Search something...'
-            className='w-full text-sm bg-transparent rounded outline-none pr-2' />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px"
-            className="cursor-pointer fill-gray-400">
-            <path
-              d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-            </path>
-          </svg>
+          className='flex xl:w-80 max-xl:w-full  bg-blue-300 cursor-pointer px-6 py-3 rounded'>
+          <button className='w-full text-sm' onClick={handleDownload}>Download</button>
         </div>
-      </div> */}
+      </div>
     </div>
   </header>
     </div>
